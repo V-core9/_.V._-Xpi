5f5fc2f5d403b8114a6e3f23ee6e7706e8b38e20
Slavko Vuletic <slavko.vuletic92@gmail.com>
2020-09-26 20:03:45 +0200

    more and more

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,5 @@ RUN apt-get update & apt-get install -y
 RUN docker-php-ext-install mysqli pdo_mysql
 
 # Enable apache mods.
-RUN a2enmod php7.4
 RUN a2enmod rewrite
 RUN a2enmod headers
--- api/users/update_user.php
+++ api/users/update_user.php
@@ -1,6 +1,10 @@
 <?php
 // required headers
-include_once '../included_files/config/header_access.php';
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
 // required to encode json web token
 include_once '../included_files/config/jwt_config.php';
