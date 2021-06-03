9feba691f8dd041111492bcd1e39a9c60526fd5d
Slavko_V <slavko.vuletic92@gmail.com>
2021-06-03 11:10:11 +0200

    yeaa Reorganize...move folders and files

--- V_XOm/API_._DockerPHP/addon_files/stop_and_clean.sh
+++ V_XOm/API_._DockerPHP/addon_files/stop_and_clean.sh
@@ -18,8 +18,8 @@ case ${answer:0:1} in
         docker volume prune -f
         echo "INFO: Pruning unused docker networks"
         docker network prune -f
-        echo "INFO: Removing directories and contents (addon_files/certs/ addon_files/certs-data/ addon_files/logs/ mysql/ PUBLIC/)"
-        rm -rf addon_files/certs/ addon_files/certs-data/ addon_files/logs/ mysql/ PUBLIC/
+        echo "INFO: Removing directories and contents (addon_files/certs/ addon_files/certs-data/ addon_files/logs/ mysql/ PUBLIC_XOMAPI/ PUBLIC_WP/)"
+        rm -rf addon_files/certs/ addon_files/certs-data/ addon_files/logs/ mysql/ PUBLIC_XOMAPI/ PUBLIC_WP/
         echo "INFO: Done"
         exit 0;
     ;;
