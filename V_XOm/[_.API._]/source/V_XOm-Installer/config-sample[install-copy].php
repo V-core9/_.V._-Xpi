189ecc201aeb5f625e6667030cc92129b20f30b2
Slavko_V <slavko.vuletic92@gmail.com>
2021-06-06 00:43:26 +0200

    yeaaaa leftovers....

--- V_XOm/[_.API._]/source/V_XOm-Installer/config-sample[install-copy].php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("DB_NAME","[>DB_NAME<]");
-define("DB_USER","[>DB_USER<]");
-define("DB_PASS","[>DB_PASS<]");
-define("DB_HOST","[>DB_HOST<]");
--- V_XOm/[_.API._]/source/V_XOm-Installer/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define( "OMEGA_LOC" , "Snksaoo1239!-5XX");
-
-define("DB_NAME","xomdb");
-define("DB_USER","root");
-define("DB_PASS","password");
-define("DB_HOST","localhost:6000");
-define("DB_PREFIX","xom_");
