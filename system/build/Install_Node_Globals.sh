6a6b441a851335ccfc1c509d7d99546be20f2fda
Slavko_V <slavko.vuletic92@gmail.com>
2021-06-12 06:14:49 +0200

    more of that reorg

---
+++ system/build/Install_Node_Globals.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Name: Global.Node.Install
+# Description: Will download, unzip and setup Xampp to be used as a server by our program
+# Author: [_.V._] 
+# Date: 20210611.05:44
+# From Node File >>  bash -c \" npm install -g fast-cli open-cli systeminfo node-notifier-cli node-notifier \"
+echo " "
+echo "*********************************************"
+echo "|| V._.XrOm-API Node Globals Installed"
+echo "*********************************************"
+echo " "
+sleep 2s
+
+WRKDIR=$PWD
+echo $WRKDIR
+echo " "
+
+# bash -c \" npm install -g fast-cli open-cli systeminfo node-notifier-cli node-notifier \" 
+
+FILENAME="./system/build/node_globals.Vinf"
+LINES=$(cat $FILENAME)
+
+for LINE in $LINES; do echo $(npm install -g $LINE); done
+
+sleep 1s
+echo " "
+echo "*********************************************"
+echo "|| V._.XrOm-API Node Globals Installed"
+echo "*********************************************"
+echo " "
+sleep 2s
