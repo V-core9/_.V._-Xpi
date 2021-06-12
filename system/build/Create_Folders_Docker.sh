7137c58eaf84e027afda16d6e45ce1a3b71119e0
Slavko_V <slavko.vuletic92@gmail.com>
2021-06-12 06:40:09 +0200

    yup

---
+++ system/build/Create_Folders_Docker.sh
@@ -0,0 +1,29 @@
+ #!/bin/bash
+# Name: Docker.containers.CreateFolderStructure
+# Description: Build docker containers, and stop the afterwards....
+# Author: [_.V._] 
+# Date: 20210611.05:44
+# From Node File >>  "_create_folders": " bash -c \" cd system ; mkdir certs ; mkdir certs-data ; mkdir logs ; cd .. ; mkdir  mysql ; cp -r SOURCE PUBLIC ; cp 'system/build/buildAPI.sh' 'PUBLIC/' \" ; ",
+echo " "
+echo "*********************************************"
+echo "|| V._.XrOm-API DOCKER Containers CreateFolderStructure "
+echo "*********************************************"
+echo " "
+
+cd system 
+mkdir certs 
+mkdir certs-data 
+mkdir logs 
+cd .. 
+mkdir  mysql 
+cp -r SOURCE PUBLIC 
+cp 'system/build/buildAPI.sh' 'PUBLIC/'
+cp 'system/config/.env_DevSample' 'PUBLIC/.env'
+cp 'system/config/.env_DevSample' './.env'
+
+echo " "
+echo "*********************************************"
+echo "|| V._.XrOm-API  DOCKER Containers CreateFolderStructure - FINISHED"
+echo "*********************************************"
+echo " "
+sleep 1s
