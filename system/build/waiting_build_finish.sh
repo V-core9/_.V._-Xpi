28bd4c5ee46d1981471617565ebbb8f40dfbddb6
Slavko_V <slavko.vuletic92@gmail.com>
2021-06-17 02:14:17 +0200

    yes remove it all...lets start from scratch....zero to hero...xD

--- system/build/waiting_build_finish.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-until $(curl --output /dev/null --silent --head --fail http://localhost:404/index.php); do
-    printf '.'
-    sleep 5
-done
-
-docker exec -it DemoName_xom bash -c " bash -c \"/var/www/html/buildAPI.sh\" ; sleep 2; echo ""; echo \"Done Setting Up PHP \" ; exit 0 ;"
